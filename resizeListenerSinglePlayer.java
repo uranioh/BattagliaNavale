ed135c1163790cdb11786878bb6e27174bab3a65
alex <alexandroscuola@gmail.com>
2023-04-22 18:51:18 +0200

    dcdcd

---
+++ resizeListenerSinglePlayer.java
@@ -0,0 +1,31 @@
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+public class resizeListenerSinglePlayer implements ComponentListener {
+    SinglePlayer s;
+    public resizeListenerSinglePlayer(SinglePlayer s){
+        this.s=s;
+    }
+
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
+    }
+}
+
