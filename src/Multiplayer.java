7d2aa9b9d9a5ba41953b2bcad6887918322407e4
AlexandroSanna <122700787+AlexandroSanna@users.noreply.github.com>
2023-05-10 09:48:17 +0200

    Delete src directory

--- src/Multiplayer.java
+++
@@ -1,9 +0,0 @@
-public class Multiplayer extends UI {
-    MainFrame frame;
-
-
-    public Multiplayer(MainFrame frame) {
-        this.frame = frame;
-        this.playGameTitle.setText("multiplayer");
-    }
-}
--- src/Singleplayer.java
+++
@@ -1,8 +0,0 @@
-public class Singleplayer extends UI {
-    MainFrame frame;
-
-    public Singleplayer(MainFrame frame) {
-        this.frame = frame;
-        this.playGameTitle.setText("singleplayer");
-    }
-}
